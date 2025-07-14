f0d02fdb22f89a7b9d706467454951ff54350b05
abeni-al7 <abenezeralebachew3@gmail.com>
2025-07-14 13:26:40 +0300

    Implement models and services

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abeni-al7/library_management
+
+go 1.24.5
--- services/library_service.go
+++ services/library_service.go
@@ -1,1 +1,79 @@
+// Define a LibraryManager interface with the following methods:
+// AddBook(book Book)
+// RemoveBook(bookID int)
+// BorrowBook(bookID int, memberID int) error
+// ReturnBook(bookID int, memberID int) error
+// ListAvailableBooks() []Book
+// ListBorrowedBooks(memberID int) []Book
 package services
+
+import (
+	"fmt"
+
+	"github.com/abeni-al7/library_management/models"
+)
+
+type LibraryManager interface {
+	AddBook(book models.Book)
+	RemoveBook(bookID int)
+	BorrowBook(bookId int, memberID int) error
+	ReturnBook(bookID int, memberID int) error
+	ListAvailableBooks() []models.Book
+	ListBorrowedBooks(memberID int) []models.Book
+}
+
+type Library struct {
+	books map[int]models.Book
+	members map[int]models.Member
+}
+
+func (library *Library) AddBook(book models.Book) {
+	id := book.ID
+	library.books[id] = book
+}
+
+func (library *Library) RemoveBook(bookID int) {
+	delete(library.books, bookID)
+}
+
+func (library *Library) BorrowBook(bookId int, memberID int) error {
+	book, ok := library.books[bookId]
+	if !ok {
+		return fmt.Errorf("book with id %v is not in the library", bookId)
+	}
+	_, exists := library.members[memberID]
+	if !exists {
+		return fmt.Errorf("member with id %v does not exist", memberID)
+	}
+
+	for _, value := range library.members {
+		taken := value.BorrowedBooks
+		for _, value := range taken {
+			if book.ID == value.ID {
+				return fmt.Errorf("this book is already borrowed out")
+			}
+		}
+	}
+
+	member := library.members[memberID]
+	member.BorrowedBooks = append(member.BorrowedBooks, book)
+	library.members[memberID] = member
+	return nil
+}
+
+func (library *Library) ListAvailableBooks() []models.Book {
+	var books []models.Book
+	for _, value := range library.books {
+		books = append(books, value)
+	}
+	return books
+}
+
+func (library *Library) ListBorrowedBooks(memberID int) []models.Book {
+	_, exists := library.members[memberID]
+	if !exists {
+		return make([]models.Book, 0)
+	}
+
+	return library.members[memberID].BorrowedBooks
+}
